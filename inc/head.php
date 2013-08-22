dd21ea63fcc8a73ac479cb7786498f60ad07a398
Valeriu Tihai <valeriu@tihai.md>
2013-08-22 11:06:58 -0400

    titre dinamyque

--- inc/head.php
+++ inc/head.php
@@ -2,7 +2,7 @@
 <!doctype html>
 <html lang="en">
 <head>
-	<title>Mini Blog</title>
+	<title><?php echo afficher_titre(); ?></title>
 	<meta charset="UTF-8" />
 <link rel="stylesheet" href="css/styles.css" type="text/css" media="all" />
 </head>
---
+++ inc/library.php
@@ -0,0 +1,30 @@
+<?php
+
+
+// title
+function afficher_titre() {
+	$file_name = trim(basename($_SERVER['PHP_SELF'], ".php").PHP_EOL);
+
+	if ($file_name == "index") {
+		$titre = "BLOG multi-usagers - TP2";
+	} 
+	elseif ($file_name == "tags") {
+		$titre = "Mots cles";
+	} 
+	elseif ($file_name == "add_edit") {
+		
+		if (isset($_GET["article"])){
+		// url structure /add_edit.php?article=modifier			
+			$titre = "Modifier billet de blog";
+		} else {
+			$titre = "Ajouter billet de blog";
+		}
+	} 
+	elseif ($file_name == "post") {
+		$titre = "Post titre";
+	} else {
+		$titre = "Blog";
+	}
+	return $titre; 
+}
+?>
