553b0f137b1a41a8e3fa1202c8c1c5fc68eb36f2
coussini <coussini@gmail.com>
2013-08-23 19:01:08 -0400

    Correction

--- inc/login.php
+++ inc/login.php
@@ -6,12 +6,12 @@
 
 		<form action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="POST" class="connecter">
 			<fieldset>
-				<label for="utilisateur">Utilisateur</label>
-				<input type="text" name="user" id="utilisateur">
+				<label for="code_usager">Code usager</label>
+				<input type="text" name="code_usager" id="code_usager">
 
-				<label for="motpasse">Mot de passe</label>
-				<input type="text" name="motpasse" id="motpasse">
-
+				<label for="mot_de_passe">Mot de passe</label>
+				<input type="password" name="mot_de_passe" id="mot_de_passe">
+				</br>
 				<input type="submit" value="S'identifier">
 			</fieldset>
 		</form>
@@ -22,3 +22,37 @@
 			<li><a href="index.php">Voir les billets de blog</a></li>
 			<li><a href="#contact">Déconnexion</a></li>
 		</ul>
+
+<?php
+	if(isset($_POST["code_usager"]) && isset($_POST["mot_de_passe"]))
+	{
+		//sélection de la base de données
+		$selection = mysqli_select_db($connectBD, "blog");	
+	
+		//envoi de la requête
+		$requete = "SELECT * from usagers where code_usager = '" 
+					. mysqli_real_escape_string($connectBD, $_POST["code_usager"]) 
+					. "' and mot_de_passe = '" 
+					. mysqli_real_escape_string($connectBD, md5($_POST["mot_de_passe"])) . "'";
+		$resultats = mysqli_query($connectBD, $requete);
+		
+		if($resultats)
+		{
+			$rangee = mysqli_fetch_assoc($resultats);
+			if($rangee)
+			{
+				afficherMessage("Bonjour " . $rangee["prenom"],false);
+			}
+			else
+			{
+				afficherMessage("Mauvaise combinaison Utilisateur et Mot de passe",true);
+			}
+		}
+		else
+			afficherMessage("Erreur de requête SQL",true);
+	}
+	else
+	{
+		afficherMessage("Vous devez inscrire le code mot de passe",true);
+	}
+?>		
