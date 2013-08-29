e994264ff1b3ef7c83f449d5577bba1d58aff4c4
Valeriu Tihai <valeriu@tihai.md>
2013-08-28 22:33:46 -0400

    date footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,3 +1,3 @@
 <div id="footer">
-	<p> 582-831-MA TP2 (Blog multi-usagers) - Louis Cyr et Valeriu Tihai.<br /> &copy; Copyright 2013</p>
+	<p> 582-831-MA TP2 (Blog multi-usagers) - Louis Cyr et Valeriu Tihai.<br /> &copy; Copyright <?php echo date('Y'); ?></p>
 </div>
